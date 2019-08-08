7c19cfe5bd8caf0d704e41af53853a28ccb1a5bc
Wojciech Lulek <w.lulek@gmail.com>
2019-08-08 10:06:07 +0200

    Adding docker-compose

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
+import time
+import redis
+
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():
         return "Hello World!"
 
+@app.route("/count")
+def visit():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=8901)
-
-
+    app.run(host='0.0.0.0',debug=True, port=8902)
