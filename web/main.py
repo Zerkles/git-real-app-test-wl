0ea2b4c8628c419203bc597d97d0ee40214ae11c
Wojciech Lulek <w.lulek@gmail.com>
2019-08-12 14:45:28 +0200

    flask and docker upgrade

--- web/main.py
+++ web/main.py
@@ -1,4 +1,5 @@
-from flask import Flask,request
+from flask import Flask, request
+
 app = Flask(__name__)
 import time
 import redis
@@ -10,11 +11,15 @@ cache = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():
-        return "Hello World!"
+    return "Hello World!"
+
+
 @app.route("/count")
 def visit():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
 def get_hit_count():
     retries = 5
     while True:
@@ -25,70 +30,87 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-#-------------------------------------------------------------------------------------
-@app.route("/rooms", methods=['POST']) #dodawanie pokoju
-def add():
-    try:
-        data=json.loads(request.data)
-        cursor.execute("""INSERT INTO Rooms VALUES (%s,%s,%s)""", (data["id"],data["name"], data["available"]))
-        connection.commit()
-        return 'Succesfully added new room!'
-    except (Exception, psycopg2.Error) as error :
-        return ("Error PostgreSQL:", error), 406
 
-@app.route("/rooms/<room_id>", methods=['DELETE']) #usuwanie pokoju
+
+@app.route("/room", methods=['POST'])
+def add():
+    # try:
+    data = json.loads(request.data)
+    name, available = "'" + str(data["name"]) + "'", data["available"]
+    cursor.execute(f"""INSERT INTO Rooms (name,available) VALUES ({name} ,{available})""")
+    connection.commit()
+    cursor.execute(f"""SELECT MAX(id) FROM Rooms WHERE name={name} AND available={available}""")
+    id = cursor.fetchall()[0][0]
+    return {"id": id, "name": name, "available": available}, 201
+
+
+# except (Exception) as error:
+# print(error)
+# return {"message": error}, 400
+
+
+@app.route("/room/<room_id>", methods=['DELETE'])
 def delete(room_id):
     try:
-        cursor.execute("DELETE from Rooms where id="+room_id)
+        cursor.execute(f"SELECT * from Rooms where id={room_id}")
+        id, name, available = cursor.fetchall()[0]
+        cursor.execute(f"DELETE from Rooms where id={room_id}")
         connection.commit()
-        return 'Succesfully deleted room!'
-    except (Exception, psycopg2.Error) as error :
-        return ("Error PostgreSQL:", error), 406
+        return {"id": id, "name": name, "available": available}
+    except (Exception) as error:
+        print(error)
+        return {"message": error}, 400
 
-@app.route("/rooms/<room_id>", methods=['GET']) #pobieranie pokoju o <id>
-def get2(room_id):
+
+@app.route("/room/<room_id>", methods=['GET'])
+def get(room_id):
     try:
-        cursor.execute("SELECT * from Rooms where id= "+str(room_id))
-        id,name,available = cursor.fetchall()[0]
-        return {"id":id,"name":str(name),"available":str(available)}
-    except (Exception, psycopg2.Error) as error :
-        return ("Error PostgreSQL:", error), 406
+        cursor.execute(f"SELECT * from Rooms where id={room_id}")
+        id, name, available = cursor.fetchall()[0]
+        return {"id": id, "name": str(name), "available": str(available)}
+    except (Exception) as error:
+        print(error)
+        return {"message": error}, 400
 
-@app.route("/rooms", methods=['GET']) #pobieranie wszystkich pokoi
-def get():
+
+@app.route("/rooms", methods=['GET'])
+def get2():
     try:
         is_available = request.args.get('available')
-        if(is_available != None and is_available == "1"):
+        if (is_available != None and is_available == "1"):
             cursor.execute("SELECT * from Rooms where available=True")
-        elif(is_available != None and is_available == "0"):
+        elif (is_available != None and is_available == "0"):
             cursor.execute("SELECT * from Rooms where available=False")
         else:
             cursor.execute("SELECT * from Rooms")
 
         result = cursor.fetchall()
-        response=[]
+        response = []
 
         for x in result:
-            id,name,available= x
-            response.append({"id":id,"name":str(name),"available":str(available)})
-        return {"rooms":response}
-    except (Exception, psycopg2.Error) as error :
-        return ("Error PostgreSQL:", error), 406
+            id, name, available = x
+            response.append({"id": id, "name": str(name), "available": str(available)})
+        return {"rooms": response}
+    except (Exception) as error:
+        print(error)
+        return {"message": error}, 400
+
 
 def connect_database():
     try:
-        connection = psycopg2.connect(user = "admin",
-            password = "admin1234",
-            host = "database",
-            port = "5432",
-            database = "post_db")
+        connection = psycopg2.connect(user="admin",
+                                      password="admin1234",
+                                      host="database",
+                                      port="5432",
+                                      database="post_db")
         print('Connected with database!')
         return connection
-    except (Exception, psycopg2.Error) as error :
-        print ("Error PostgreSQL:", error)
+    except (Exception) as error:
+        print(error)
+        return {"message": error}, 400
+
 
 if __name__ == "__main__":
     connection = connect_database()
     cursor = connection.cursor()
-    app.run(host='0.0.0.0',debug=True, port=8903)
-
+    app.run(host='0.0.0.0', debug=True, port=8903)
